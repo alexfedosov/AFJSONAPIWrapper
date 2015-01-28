27f06375400b58b1513c39f49cc396570b49532e
Alexander Fedosov <alexander.a.fedosov@gmail.com>
2015-01-28 12:12:29 +0300

    Initial Commit

---
+++ AFJSONAPIWrapper/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AFJSONAPIWrapper
+//
+//  Created by Alexaner Fedosov on 28.01.15.
+//  Copyright (c) 2015 alexfedosov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
